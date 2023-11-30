ff30a5788509b5df8c84a9a263d3e3983fa7a179
au-lex <aulexc4d2@gmail.com>
2023-11-30 20:29:26 +0100

    side navigation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/images/logo_prev_ui.png'
 import { CgMenuRight } from "react-icons/cg";
 import { FaBagShopping } from "react-icons/fa6";
+import { SlClose } from "react-icons/sl";
+import { SiIfood } from "react-icons/si";
+import { TbSmartHome } from "react-icons/tb"
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineInfo } from "react-icons/md";
+import { IoMdContacts } from "react-icons/io";
+import { RiLogoutCircleLine } from "react-icons/ri";
 const Header = () => {
+
+
+    const [openSidebar, setOpenSidebar] = useState(false)
+    const toggleSidebar =() => {
+        setOpenSidebar(!openSidebar) 
+    }
   return (
     <>
 {/* mobile-header */}
-    <header className='flex justify-between  px-[.8rem] pt-[1.2rem]'>
+    <header className='flex justify-between  px-[.8rem] pt-[.6rem]'>
 
 <section className='flex'>
 
@@ -15,16 +28,69 @@ const Header = () => {
     </div>
 
 <h2 className='font-semibold'>Foodie
- <span className='text-yellow-500 font-bold text-[2rem]'>.oi</span></h2>
+ <span className='text-yellow-500 font-bold text-[1.5rem]'>.oi</span></h2>
 </section>
 
-    <nav className='flex pt-[1rem] space-x-8'>
+    <nav className='flex pt-[1rem] space-x-12 '>
         <div><FaBagShopping className='text-[1.6rem] text-yellow-500 cursor-pointer '  /></div>
 <div>
-    <CgMenuRight className='text-[1.9rem] text-yellow-500 cursor-pointer ' />
+    <CgMenuRight className='text-[2.2rem]
+     text-yellow-500 cursor-pointer ' onClick={toggleSidebar} />
 </div>
+
     </nav>
     </header>
+    
+<div className={`sidebar w-[100%] pt-[2rem] px-[1rem] fixed ${openSidebar ? "active": ""}  bg-white a z-50 top-0 bottom-0`}>
+<section className='flex'>
+
+    <div className="logo w-[60px] h-[50px]">
+<img src={logo} alt="logo" className='w-[100%]' />
+    </div>
+
+<h2 className='font-semibold'>Foodie
+ <span className='text-yellow-500 font-bold text-[1.5rem]'>.oi</span></h2>
+</section>
+
+<span onClick={toggleSidebar}>< SlClose className='text-[2rem] absolute right-6 
+top-6' /></span>
+<div className="profile py-[2rem]  flex space-x-2 mt-[0rem] shadow-md">
+    <div className="image flex items-center w-[80px] h-[80px]
+     rounded-full bg-slate-300 "></div>
+     <div>
+
+     <p className=' mt-[.7rem] font-medium'>John Doe</p>
+     <p className=' font-medium'>aulexc4d2@gmail.com</p>
+     </div>
+
+</div>
+
+<ul className='pt-[1rem]' >
+
+
+    <li className='text-[1.5rem] flex mb-[2.5rem] font-semibold' > <span className='pr-2'>
+        <TbSmartHome className='text-[2rem] text-yellow-600  ' /></span>Home</li>
+    <li className='text-[1.5rem] flex  mb-[2.5rem] font-semibold ' > <span className='pr-2'>
+        <SiIfood className='text-[2rem] text-yellow-600 ' /></span> Food Menu</li>
+    <li className='text-[1.5rem] flex  mb-[2.5rem] font-semibold' > <span className='pr-2'>
+        <MdOutlineInfo className='text-[2rem]  text-yellow-600' />
+        </span>About us </li>
+    <li className='text-[1.5rem] flex  mb-[2.5rem] font-semibold' > <span className='pr-2'>
+        <IoMdContacts className='text-[2rem] text-yellow-600 ' /></span>Contact us</li>
+    <li className='text-[1.5rem] flex  mb-[1.8rem] font-semibold' > <span className='pr-2'>
+        <IoSettingsOutline className='text-[2rem] text-yellow-600 ' /></span>Settings</li>
+
+
+        <div className="logout left-0  bottom-0 w-full">
+    <button type="submit" className="bg-yellow-900 hover:bg-red-
+    600  h-[4rem] w-full text-white font-bold uppercase border-none rounded-lg">
+       Log out</button>  
+</div>
+
+</ul>
+
+
+</div>
  
     </>
   )
